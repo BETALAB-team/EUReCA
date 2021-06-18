ad013318a8af4af094c1d26049ba7773bccbf08d
enr-prt <enrico.prataviera.lav@gmail.com>
2021-06-18 14:34:43 +0200

    Just a change in an input file

---
+++ Main_test_DHW.py
@@ -0,0 +1,40 @@
+''' IMPORTING MODULES '''
+
+import os
+import numpy as np
+from RC_classes.Simulation import Sim
+
+
+# Creation of the Sim objectexit
+city = Sim()
+
+# Loading the input data
+city.set_input_from_text_file(os.path.join('.','Input','SimInput'))
+city.set_input_from_excel_file(os.path.join('.','Input','SimInput.xlsx'))
+
+# Loading weather data, envelopes and schedules
+city.preprocessing()
+
+# Creation of the district (geometrical processing)
+city.city_creation()
+
+# Evaluating Urban shadings between buildings
+city.surfaces_and_shading()
+
+# Calculation buildings parameters
+city.buildings_params_and_loads()
+
+# Evaluating Urban shadings between buildings
+city.urban_canopy()
+
+# Design power of buildings and plants creation
+city.plants_design_and_creation()
+
+# Annual simulation
+city.simulation()
+
+# Output processing
+city.output()
+
+
+# %%
