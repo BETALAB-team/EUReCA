2d70bda9deb6850aa5b462ba2b4f99b891786e32
pratenr82256 <enrico.prataviera.lav@gmail.com>
2023-08-03 11:19:09 +0200

    Comment matplotlib in main

--- eureca_building/building.py
+++ eureca_building/building.py
@@ -299,7 +299,9 @@ Please run thermal zones design_sensible_cooling_load and design_heating_load
         if output_folder != None:
             if not os.path.isdir(output_folder):
                 os.mkdir(output_folder)
-            total.to_csv(os.path.join(output_folder, f"Results {self.name}.csv"), float_format='%.2f', index = False, sep =";")
+            # total.to_csv(os.path.join(output_folder, f"Results {self.name}.csv"), float_format='%.2f', index = False, sep =";")
+            total.to_parquet(os.path.join(output_folder, f"Results {self.name}.parquet.snappy"), engine="pyarrow", compression = "snappy")
+            # total.to_hdf(os.path.join(output_folder, f"Results {self.name}.h5"), key='total', mode='w')
 
         return total
 
