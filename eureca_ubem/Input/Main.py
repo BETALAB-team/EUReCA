809b3aa2a9e48ad9095003859c632c7952caa4c6
pratenr82256 <enrico.prataviera.lav@gmail.com>
2023-10-20 10:42:32 +0200

    Update to include a print function for the surface object

--- eureca_ubem/Input/Main.py
+++ eureca_ubem/Input/Main.py
@@ -31,7 +31,7 @@ city_model_file = os.path.join(".","PiovegoRestricted_with_holes.geojson")
 # )
 # city_geojson.loads_calculation(region="Veneto")
 # city_geojson.simulate(print_single_building_results=True)
-
+#
 materials_file = os.path.join(".","total envelope types.xlsx")
 city_model_file = os.path.join(".","Belzoni_2023_July_Update.json")
 
