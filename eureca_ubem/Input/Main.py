39841c32998ce4f5e9b67af97831720448a47d65
Mohamad Hasan Khajedehi <mohamadhasan.khajedehi@studenti.unipd.it>
2024-07-01 09:19:53 +0200

    final check

--- eureca_ubem/Input/Main.py
+++ eureca_ubem/Input/Main.py
@@ -17,8 +17,8 @@ from eureca_ubem.city import City
 
 weather_file = os.path.join(".","ITA_Venezia-Tessera.161050_IGDG.epw")
 schedules_file = os.path.join(".","Schedules.xlsx")
-materials_file = os.path.join(".","materials_and_construction_test.xlsx")
-city_model_file = os.path.join(".","PiovegoRestricted_with_holes_corr_coef.geojson")
+materials_file = os.path.join(".","total envelope types.xlsx")
+city_model_file = os.path.join(".","Padova_City_Center.geojson")
 
 city_geojson = City(
     city_model=city_model_file,
