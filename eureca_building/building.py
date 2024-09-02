111a927a6fc130bb6e7c353a16f3068f217a8c93
Mohamad Hasan Khajedehi <147405956+Mohamad-k97@users.noreply.github.com>
2024-09-02 10:33:40 +0200

    Comment Results of TS

--- eureca_building/building.py
+++ eureca_building/building.py
@@ -288,9 +288,9 @@ Please run thermal zones design_sensible_cooling_load and design_heating_load
             'TZ DHW volume flow rate [L/s]' : np.zeros([CONFIG.number_of_time_steps, len(self._thermal_zones_list)]),
             'TZ DHW demand [W]' : np.zeros([CONFIG.number_of_time_steps, len(self._thermal_zones_list)]),
 
-            'DHW tank charging mode [-]' : np.zeros([CONFIG.number_of_time_steps, 1]),
-            'DHW tank charge [-]' : np.zeros([CONFIG.number_of_time_steps, 1]),
-            'DHW tank charging rate [W]' : np.zeros([CONFIG.number_of_time_steps, 1]),
+            #'DHW tank charging mode [-]' : np.zeros([CONFIG.number_of_time_steps, 1]),
+            #'DHW tank charge [-]' : np.zeros([CONFIG.number_of_time_steps, 1]),
+            #'DHW tank charging rate [W]' : np.zeros([CONFIG.number_of_time_steps, 1]),
 
             'Storage Tank Charge [%]' : np.zeros([CONFIG.number_of_time_steps, 1]),
             'Solar Thermal Production [Wh]' : np.zeros([CONFIG.number_of_time_steps, 1]),
@@ -338,9 +338,9 @@ Please run thermal zones design_sensible_cooling_load and design_heating_load
             results['TZ AHU electric load [W]'][t - t_start, :] = [tz.AHU_electric_consumption for tz in
                                                                       self._thermal_zones_list]
 
-            results['DHW tank charging mode [-]'][t - t_start, 0] = self.heating_system.charging_mode
-            results['DHW tank charge [-]'][t - t_start, 0] = self.heating_system.dhw_tank_current_charge_perc
-            results['Solar Thermal Production [Wh]'][t - t_start,0] = self.heating_system.solar_thermal_gain
+            #results['DHW tank charging mode [-]'][t - t_start, 0] = self.heating_system.charging_mode
+            #results['DHW tank charge [-]'][t - t_start, 0] = self.heating_system.dhw_tank_current_charge_perc
+            #results['Solar Thermal Production [Wh]'][t - t_start,0] = self.heating_system.solar_thermal_gain
             results['Non-Renewable DHW [Wh]'][t - t_start,0] = self.heating_system.dhw_capacity_to_tank
             results['Heating system gas consumption [Nm3]'][t - t_start,0] = self.heating_system.gas_consumption
             results['Heating system oil consumption [L]'][t - t_start,0] = self.heating_system.oil_consumption
