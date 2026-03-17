c6adbbf8d92124997b315ed02c473d2a37f1f779
Mohamad Hasan Khajedehi <147405956+Mohamad-k97@users.noreply.github.com>
2026-03-17 14:08:03 +0100

    Change units for CO2 and energy metrics
    Adjust CO2 and renewable energy calculations for 'Grid Electricity'.

--- eureca_ubem/city.py
+++ eureca_ubem/city.py
@@ -866,11 +866,11 @@ Lazio, Campania, Basilicata, Molise, Puglia, Calabria, Sicilia, Sardegna
             co2_s3_f = district_hourly_results[f"{f} CO2 Emission Scope 3 [ton CO2]"].sum()
 
             if f == "Gird Electricity":
-                pe_ren_f=pe_ren_f/1000
-                pe_nren_f=pe_nren_f/1000
-                co2_s1_f=co2_s1_f
-                co2_s2_f=co2_s2_f
-                co2_s3_f=co2_s3_f
+                pe_ren_f=pe_ren_f/1000000
+                pe_nren_f=pe_nren_f/1000000
+                co2_s1_f=co2_s1_f/1000
+                co2_s2_f=co2_s2_f/1000
+                co2_s3_f=co2_s3_f/1000
         
             rows.append([
                 f,
